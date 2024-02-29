12d7f875d3defc9b3b8b05341dbec185f7e3f4f7
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-02-29 10:46:51 +0200

    Remove unused imports in nav component

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,9 +2,6 @@ import Link from 'next/link';
 import axios from 'axios';
 import { useState, useEffect } from "react";
 import { supabase } from '../lib/supabaseClient';
-import { Session } from "@supabase/supabase-js";
-
-
 
 const Nav = () => {
  
